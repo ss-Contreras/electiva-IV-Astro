e6d7a4288161c30fcea725edcc593216544e4a8d
Sergio Contreras <sergiocontreras2003ss@gmail.com>
2024-08-21 01:14:25 -0500

    Update DB

--- db/seed.ts
+++ db/seed.ts
@@ -22,5 +22,25 @@ export default async function () {
       motivoConsulta: "Revisión general",
       rutaImagenes: "images/maria_gonzales.png",
     },
+    {
+      nombre: "Sergio Contreras",
+      cedula: "1006875130",
+      edad: 21,
+      fechaCita: new Date('2024-08-22'),
+      pacienteNuevo: false,
+      pacienteRecomendado: false,
+      motivoConsulta: "Me duele el pie",
+      rutaImagenes: "images/person.png",
+    },
+    {
+      nombre: "Pepito Pérez",
+      cedula: "0987654321",
+      edad: 40,
+      fechaCita: new Date('2024-08-22'),
+      pacienteNuevo: false,
+      pacienteRecomendado: true,
+      motivoConsulta: "Revisión general",
+      rutaImagenes: "images/person2.png",
+    },
   ]);
 }
