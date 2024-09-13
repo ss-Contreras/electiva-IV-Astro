d69d4102488c0a28a055a3264b509b9eca996e51
XxNC10xX <nicolascabrera.18@hotmail.com>
2024-09-12 19:20:07 -0500

    Actualizacion errores

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import db from "@astrojs/db";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db()]
+  integrations: [tailwind()],
 });
